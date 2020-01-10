17a5b546ba567b34aba0d63821c7763950b036c6
Harland Williams <harland@cdrsoftware.com>
2020-01-10 10:09:38 -0600

    Set up app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 
 
+
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -15,7 +16,7 @@ const location = search.value
 
 messageOne.textContent = 'Loading . . .'
 
-fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             messageOne.textContent = data.error
